aad42a0666bef593fcb7b9dfd10ebd212ee056c1
S.watana <75829584+opepelab@users.noreply.github.com>
2021-11-05 20:17:11 +0900

    Add files via upload

---
+++ ActiveLink.tsx
@@ -0,0 +1,28 @@
+//Git Hub@remy氏 (EN)
+//Qiita @TK-C (JA)
+
+import { withRouter, NextRouter  } from 'next/router';
+import React, { ReactElement } from 'react';
+import Link from 'next/link';
+
+type Props = {
+  router: NextRouter;
+  children: ReactElement;
+  href: string;
+  activeClassName: string;
+}
+
+const ActiveLink = ({ router, children, ...props }: Props) => {
+  const child = children;
+  
+  let className: string = child.props.className;
+  if (router.pathname == props.href) {
+    className = `${className} ${props.activeClassName}`;
+  }
+
+  return (
+  <Link {...props}>{React.cloneElement(child, { className })}</Link>
+  );
+}
+
+export default withRouter(ActiveLink);
